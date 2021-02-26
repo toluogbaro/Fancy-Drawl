b7af11a2b053d2317499a823b4a1760f4e6a0036
Tolu Ogbaro <toluogbaro123@gmail.com>
2021-02-26 06:23:04 +0000

    Setting up chatbot AI
    - Added UI
    - Begun Implementation of code

---
+++ Fancy-Drawl-Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using Syn.Bot.Oscova;
+using Syn.Bot.Oscova.Attributes;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum MessageType
+{
+    User,
+    Bot
+}
+
+public class Message
+{
+    public string text;
+    public Text textObject;
+    public MessageType messageType;
+
+}
+
+public class GameManager : MonoBehaviour
+{
+    OscovaBot oscovaBot;
+
+    
+}
+
+
+
+
